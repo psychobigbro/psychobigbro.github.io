79e7c7243f88bad28edae8ad776521a5060cf2aa
psychobigbro <36944678+psychobigbro@users.noreply.github.com>
2018-03-11 20:58:15 +0800

    32

--- work/webapps.js
+++ work/webapps.js
@@ -10,7 +10,7 @@ function callGoogleScript(func,
   	if (param === undefined) param = "";
 	if (successHandler === undefined) successHandler = onSuccess; //default
 		var url =  JSONPTestExec + "func=" + func + "&param=";
-		if ($(#test-mode_switch).value("test-mode") == "off")
+		if ($(#test-mode-switch).value("test-mode") == "off")
 			url =  HKRaceExec + "func=" + func + "&param=";	
 		var request = $.ajax({
 			crossDomain: true,
