d5539069fdd118df6cc29cdb42f27fa22f460afe
psychobigbro <36944678+psychobigbro@users.noreply.github.com>
2018-03-13 15:29:13 +0800

    88

--- work/webapps.js
+++ work/webapps.js
@@ -90,9 +90,11 @@ function onDeleteTimeTriggersSuccess (result,status,xhr) {
 		//ask jqm to enhance all checkboxes and controlgroup
 		$("[type=checkbox]").checkboxradio();
 		$("[data-role=controlgroup]").controlgroup("refresh");
-	}
-	$fieldset.find("h3").html("Check to delete triggers:");
-	$("#del-trigger-btn").removeAttr("disabled");  //enable delete btn
+		$fieldset.find("h3").html("Check to delete triggers:");
+		$("#del-trigger-btn").removeAttr("disabled");  //enable delete btn
+	} else
+		$fieldset.find("h3").html("No triggers left!");
+	
 	//$( "#triggerDialog" ).find("[href]").removeAttr("disabled"); //enable anchor close btn
 }
 
